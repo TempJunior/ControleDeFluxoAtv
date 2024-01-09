a1af184160e15dad1b34e5ffcfa269508994a1e2
JuniorTemp <junior_sadaanzinhoa@hotmail.com>
2024-01-09 12:54:16 -0300

    first commit

---
+++ ControleDeFluxoAtv/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        System.out.println("Digite sua nota: ");
+        int Nota = s.nextInt();
+        String Notast = getNota(Nota);
+         System.out.println(Notast);
+    }
+        public static String getNota (int Nota){
+            if (Nota >=7){
+                return "Aprovado";
+            } else if (Nota >=5 && Nota <=7){
+                return "Está em recuperação!";
+            }else {
+                return "Está reprovado!";
+            }
+
+
+
+        }
+
+}
